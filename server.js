40c46ed591f14037441be7350c46f9fe9284012b
Nevrard <ndangaevrard@gmail.com>
2021-06-30 22:40:48 -0400

    create api routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const router = require("./routes/api.js");
+const router = require("./routes/apiRoutes.js");
 
 const PORT = process.env.PORT || 3000;
 
@@ -26,7 +26,7 @@ let db = mongoose.connect(
 
 // routes
 app.use(router);
-require("./routes/html.js")(app);
+require("./routes/htmlRoutes.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
