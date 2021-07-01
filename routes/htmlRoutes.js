bc908eaa5146357275560ae71ef1814555bd9ceb
Nevrard <ndangaevrard@gmail.com>
2021-06-30 22:23:30 -0400

    create html route- home page

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const path = require("path");
+
+module.exports = function (app) {
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
